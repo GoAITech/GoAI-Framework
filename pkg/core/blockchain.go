5252b045f58ba5cefd6f5eae56673aab44154be3
GoAITech <Dev@goaitech.xyz>
2025-03-04 09:09:13 +0100

    Update blockchain.go

---
+++ pkg/core/blockchain.go
@@ -0,0 +1,9 @@
+package core
+
+import "fmt"
+
+// VerifyModelIntegrity ensures AI models are tamper-proof using blockchain
+func VerifyModelIntegrity(modelHash string) bool {
+	fmt.Printf("Verifying AI model integrity: %s\n", modelHash)
+	return true
+}
